d4c70d177240111c76a573db7caf177d007cc631
kaja <kaja.d111@gmail.com>
2024-11-02 16:14:57 +0100

    add representation to models

--- tournaments/models.py
+++ tournaments/models.py
@@ -1,12 +1,19 @@
 from django.db import models  
 
+
 class Player(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
     
 
 class Tournament(models.Model):
     name = models.CharField(max_length=200)
     players = models.ManyToManyField(Player)
+
+    def __str__(self):
+        return self.name
 
     
 class Game(models.Model):
@@ -15,4 +22,10 @@ class Game(models.Model):
     first_player_goals = models.IntegerField(blank=True, null=True)
     second_player_goals = models.IntegerField(blank=True, null=True)
     tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_player.name} {self.first_player_goals or '_'} - {self.second_player_goals or '_'} {self.second_player.name}"
+
+    def finished(self):
+        return self.first_player_goals && self.second_player_goals
     
